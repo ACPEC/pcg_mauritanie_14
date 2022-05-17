f9ce08d073e82f0aad204cadecfb3b3604a37cae
Sidi Hmety <sidi.ehmety@gmail.com>
2022-05-17 19:05:59 +0000

    Plan comptable - Mauritanie Odoo 14

---
+++ acpec_l10n_mr/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2010 kazacube (http://kazacube.com).
+
+{
+    'name': 'Plan Comptable de la Mauritanie',
+    'author': 'Med Mahmoud',
+    'company': 'ACPEC SARL',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'website': 'https://www.acpec-sarl.com',
+    'depends': ['base', 'account'],
+    'description': u"""
+    Ce module charge un modèle de plan de comptes généraux pour la Mauritanie.
+    Il offre un ensemble réduit de comptes et de taxes suffisant pour démarrer avec ODOO. Instructions d'installation:
+    Créer une base Odoo vierge. Installer ce module et puis installer le module de la comptabilité. Et c'est tout!""",
+    'data': [
+        'data/l10n_mr_chart_data.xml',
+        'data/account_chart_template_data.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'auto_install': False,
+}
